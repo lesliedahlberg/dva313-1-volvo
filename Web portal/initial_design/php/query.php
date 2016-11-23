efbf21352688141a7776f0d2ffa5fdd1c7239fe7
vladodjuka <v.djuka95@gmail.com>
2016-11-23 13:44:44 +0100

    connect database and gui

--- Web portal/initial_design/php/query.php
+++ Web portal/initial_design/php/query.php
@@ -3,16 +3,18 @@
 include "connection.php";
 
 
-if(isset($_GET["getData"])){
-   getHighscores();
-}
+//if(isset($_POST["getData"])){
+//   getHighscores();
+//}
 
 
-function getHighscores(){
+//function getHighscores(){
 $queryy="SELECT s.id,s.score,a.name,s.timePlayed,s.duration,GROUP_CONCAT(DISTINCT v.time order by v.time asc) TIME, GROUP_CONCAT(case when v.type='Fuel' then v.value end ORDER BY v.time asc SEPARATOR ',') FUEL,GROUP_CONCAT(case when v.type='RPM' then v.value end ORDER BY v.time asc SEPARATOR ',') RPM FROM Session as s
 JOIN Alias as a ON (a.id = s.IdAlias) JOIN ValueTime as v ON (v.IdSession=s.id) GROUP BY s.id order by s.score desc LIMIT 20";
 
 $result = mysqli_query($connection, $queryy);
+
+//echo $result;
 
 $emparray = array();
 
@@ -20,11 +22,12 @@ while ($row = mysqli_fetch_assoc($result)){
 $emparray[] = $row;
 }
 
-echo json_encode($emparray);
+$json = json_encode ( $emparray ); //JSON_FORCE_OBJECT
+echo $json;
 
 //echo $emparray;
 
-}
+//}
 
 
 mysqli_close($connection);
