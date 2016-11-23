6cab7ee89cb3b7b8baab0f0bfc5062deadbd4a13
Engtorp <eengtorp@gmail.com>
2016-11-23 09:02:54 +0100

    Alias activity start

---
+++ App/Volvo/app/src/main/java/softproduct/volvo/com/eco_drive/AliasActivity.java
@@ -0,0 +1,13 @@
+package softproduct.volvo.com.eco_drive;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AliasActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alias);
+    }
+}
