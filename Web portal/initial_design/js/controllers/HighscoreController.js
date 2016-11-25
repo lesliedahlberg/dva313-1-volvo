b8f9bf5f23b7f5fe346e24e8bf483bcc2e706faa
vladodjuka <v.djuka95@gmail.com>
2016-11-25 18:24:50 +0100

    scroll to active highscore

--- Web portal/initial_design/js/controllers/HighscoreController.js
+++ Web portal/initial_design/js/controllers/HighscoreController.js
@@ -2,115 +2,149 @@
 var HighscoreController = function(radialChartController, lineChartController){
 
 
-		var fail = function(){
+	var fail = function(){
 
-			$('.modal').modal();
-			$('#error-modal').modal('open');
+		$('.modal').modal();
+		$('#error-modal').modal('open');
 
-		}
-		var done = function(data){
+	}
+	var done = function(data){
 
-			var generatedView = "";
+		var generatedView = "";
 
-			for (var i = 0; i < data.length; i++) {
-				data[i].index = i;
-				generatedView += generateView(data[i]);
-			};
+		for (var i = 0; i < data.length; i++) {
+			data[i].index = i;
+			generatedView += generateView(data[i]);
+		};
 
-			$('#results-table').html(generatedView);
+		$('#results-table').html(generatedView);
 
-			for (var i = 0; i < data.length; i++) {
-				radialChartController.init(data[i]);	
-				lineChartController.init(data[i]);
-			};
+		for (var i = 0; i < data.length; i++) {
+			radialChartController.init(data[i]);	
+			lineChartController.init(data[i]);
+		};
 
 
-		onComplete();	
+	onComplete();	
 }
 
-	function onComplete(){
-		$('body').addClass('loaded');
-		$(".hhhd:first").addClass('active');
-		$('.collapsible').collapsible({
-			onOpen: function(el) { }
-		});
+function onComplete(){
+	$('body').addClass('loaded');
+	$(".hhhd:first").addClass('active');
+	$('.collapsible').collapsible({
+		onOpen: function(el) { 
+			
+		}
+	});
+	$(".collapsible-header").on("click", function(e){
+		 e.preventDefault();
+		var contentPanelId = jQuery(this).attr("id");
+		var classHeader = jQuery(this).attr("class");
+		classHeader=classHeader.split(" ");
+		
+		 if(classHeader.length<3){
+		setTimeout(function(){
+			
+			var completeS="#"+contentPanelId;
+// alert(contentPanelId);
+
+$('html, body').animate({scrollTop:$(completeS).position().top}, 'normal');
+			//window.location.hash=completeS;
+       // window.scrollTo(200, 100);
+    }, 300);
 	}
+
+	});
+
+}
+
+
+
+
+$.fn.focusWithoutScrolling = function(){
+var x = window.scrollX, y = window.scrollY;
+alert(x +" "+ y);
+this.focus();
+window.scrollTo(x, y);
+// return this; //chainability
+};
+
 
 function getData(){
 
-	$.ajax({
-		type: "POST",
-		url: "http://autoelektronikame.ipage.com/volvo/php/query.php",
-		dataType : 'json',
-		error: fail,
-		success: done
-	});
+$.ajax({
+	type: "POST",
+	url: "http://autoelektronikame.ipage.com/volvo/php/query.php",
+	dataType : 'json',
+	error: fail,
+	success: done
+});
 }
 
 var init = function (){
 
-	getData();
+getData();
 }
 
 return {
-	init : init
+init : init
 }
 
 
 function generateView (item){
 
 
-	var html = `
-	<li>
-	<!--Header of the item-->
-	<div class="collapsible-header hhhd">
-	
-	{{name}}</span> 
-	<span class="right score">Score: {{score}}</span>
+var html = `
+<li>
+<!--Header of the item-->
+<div class="collapsible-header hhhd" id="{{index}}">
+
+{{name}}</span> 
+<span class="right score">Score: {{score}}</span>
+</div>
+<!-- Body of first collapsable item -->
+<div class="collapsible-body">
+
+<div class="row">
+
+<!--LEFT SIDE-->
+<div class="col s6">
+<br/><br/><br/>
+<div class="radar-chart-container">
+<canvas id="radar-chart-{{index}}" class="radar-chart" ></canvas>
+</div>
+</div>
+<!-- END LEFT SIDE-->
+
+  <!-- BEGINING // CHART, BUTTONS, TABLE-->
+  <div class="col s6">
+  <!--LINE CHART-->
+  <canvas id="line-chart-{{index}}" width="200" height="150" style="margin-top:9px;"></canvas>
+  <!--END LINE CHART-->
+  <!--BUTTONS -->  
+
+  <a class="waves-effect waves-light btn">Fuel</a>
+  <a class="waves-effect waves-light btn">Distance</a>
+  <a class="waves-effect waves-light btn">Load</a>
+
+  <!--END BUTTONS -->
+
+	<!-- START // TABLE-->
+	<ul class="collection" style="margin-top:9px;">
+
+	<li class="collection-item">Fuel: {{totalFuel}} L</li>
+	<li class="collection-item">Distance: {{totalDistance}} km</li>
+	<li class="collection-item">Load: {{totalLoad}} kg</li>
+	</ul>
+	<!-- END // TABLE-->
 	</div>
-	<!-- Body of first collapsable item -->
-	<div class="collapsible-body">
+  <!-- END // CHART, BUTTONS, TABLE-->
+  </div>
+  </li>
+  `;
 
-	<div class="row">
-
-	<!--LEFT SIDE-->
-	<div class="col s6">
-	<br/><br/><br/>
-	<div class="radar-chart-container">
-	<canvas id="radar-chart-{{index}}" class="radar-chart" ></canvas>
-	</div>
-	</div>
-	<!-- END LEFT SIDE-->
-
-	  <!-- BEGINING // CHART, BUTTONS, TABLE-->
-	  <div class="col s6">
-	  <!--LINE CHART-->
-	  <canvas id="line-chart-{{index}}" width="200" height="150" style="margin-top:9px;"></canvas>
-	  <!--END LINE CHART-->
-	  <!--BUTTONS -->  
-
-	  <a class="waves-effect waves-light btn">Fuel</a>
-	  <a class="waves-effect waves-light btn">Distance</a>
-	  <a class="waves-effect waves-light btn">Load</a>
-
-	  <!--END BUTTONS -->
-
-		<!-- START // TABLE-->
-		<ul class="collection" style="margin-top:9px;">
-
-		<li class="collection-item">Fuel: {{totalFuel}} L</li>
-		<li class="collection-item">Distance: {{totalDistance}} km</li>
-		<li class="collection-item">Load: {{totalLoad}} kg</li>
-		</ul>
-		<!-- END // TABLE-->
-		</div>
-	  <!-- END // CHART, BUTTONS, TABLE-->
-	  </div>
-	  </li>
-	  `;
-
-	  return Mustache.render(html, item);; 
-	}
+  return Mustache.render(html, item);; 
+}
 
 
 
