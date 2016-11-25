89e2e61c10777c9043be7ba683dd121b8bac6e3a
HamzaSabljakovic <hamza.sabljakovic.fit@gmail.com>
2016-11-25 19:15:10 +0100

    test push android

--- App/Volvo/app/src/main/java/softproduct/volvo/com/eco_drive/GameActivity.java
+++ App/Volvo/app/src/main/java/softproduct/volvo/com/eco_drive/GameActivity.java
@@ -12,5 +12,7 @@ public class GameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_display);
+        //hehehe
+        int i = 69;
     }
 }
