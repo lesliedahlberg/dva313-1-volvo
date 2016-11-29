f3f36d7b30a6ea7bd97de5696f413ebccc472839
vladodjuka <v.djuka95@gmail.com>
2016-11-29 13:23:07 +0100

    Create new game and insert  valuetime parameters

--- Web portal/initial_design/php/create-new-game.php
+++ Web portal/initial_design/php/create-new-game.php
@@ -6,13 +6,35 @@ include "connection.php";
 //$alias = $_POST["alias"]
 
 $time = '12:00'; 
-//$alias = $_POST["alias"]
+$alias = $_POST["alias"];
 
-$aliasID = 4;
+//$aliasID = 4;
 $idmachine=1;
 
-$sql = "INSERT INTO Session ( TimePlayed, IdAlias, IdMachine) VALUES ( '$time', $aliasID, $idmachine)";
+ $sql = $connection->prepare("SELECT id from Alias WHERE name=?");
+$sql->bind_param('s', $alias);
+
+$sql->execute();
+
+$result = $sql->get_result();
+
+$idAlias="";
+if(mysqli_num_rows($result)!=0){
+    $data=$result->fetch_assoc();
+    $idAlias=$data["Id"];
+}
+else{
+	 $sqlQuerry = "INSERT INTO Alias (Name) values('$alias')";
+	 $result = mysqli_query($connection, $sqlQuerry);
+	 $idAlias=$connection->insert_id;
+
+}
+
+
+
+$sql = "INSERT INTO Session ( TimePlayed, IdAlias, IdMachine) VALUES ( '$time', $idAlias, $idmachine)";
 $result = mysqli_query($connection, $sql);
+
 
 echo $connection->insert_id;
 
--- Web portal/initial_design/php/register-event.php
+++ Web portal/initial_design/php/register-event.php
@@ -2,21 +2,20 @@
 
 include "connection.php";
 
-//$time = $_POST["time"]; 
-//$alias = $_POST["alias"]
+$time = $_POST["time"]; 
+$value = $_POST["value"]; 
+$type = $_POST["type"];
+$sessionId = $_POST["session"];
 
-$time  = '12:00'; 
-$value = 5;
-$type  = 'FUEL';
-
-$sessionId  = 9;
 
 $sql = "INSERT INTO ValueTime ( Type,  Value, Time,  IdSession) 
 					   VALUES ( '$type', $value,'$time', $sessionId)";
 
-echo $sql;
 
 $result = mysqli_query($connection, $sql);
+
+echo $connection->insert_id;
+
 
 
 mysqli_close($connection);
