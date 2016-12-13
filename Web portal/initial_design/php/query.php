2cbcaab2d10548465e0ff69278e7f2264e9f96c8
vladodjuka <v.djuka95@gmail.com>
2016-12-13 23:09:17 +0100

    Round on one decimal

--- Web portal/initial_design/php/query.php
+++ Web portal/initial_design/php/query.php
@@ -14,22 +14,22 @@ $data="".$_POST['getData']."";
 
 
 
-$queryy="SELECT s.id,s.score,a.name,s.timePlayed,s.duration, m.name as machine, (select sum(vtt.value) from ValueTime as vtt where vtt.type='Fuel' and vtt.IdSession=s.id) as totalFuel,
-(select sum(vtt.value) from ValueTime as vtt where vtt.type='DISTANCE' and vtt.IdSession=s.id) as totalDistance,
-(select sum(vtt.value) from ValueTime as vtt where vtt.type='LOAD' and vtt.IdSession=s.id) as totalLoad,
-(select ROUND(avg(vtt.value),0) from ValueTime as vtt where vtt.type='Fuel' and vtt.IdSession=s.id) as averageFuel,
-(select ROUND(avg(vtt.value),0) from ValueTime as vtt where vtt.type='DISTANCE' and vtt.IdSession=s.id) as averageDistance,
-(select ROUND(avg(vtt.value),0) from ValueTime as vtt where vtt.type='LOAD' and vtt.IdSession=s.id) as averageLoad,
-(select ROUND(avg(vtt.value),0) from ValueTime as vtt where vtt.type='Speed' and vtt.IdSession=s.id) as averageSpeed, 
-(select ROUND(avg(vtt.value),0) from ValueTime as vtt where vtt.type='RPM' and vtt.IdSession=s.id) as averageRpm,    
-(select ROUND(avg(vtt.value),0) from ValueTime as vtt where vtt.type='ALTITUDE' and vtt.IdSession=s.id) as averageAltitude,
+$queryy="SELECT s.id,s.score,a.name,s.timePlayed,s.duration, m.name as machine, (select ROUND(sum(vtt.value),1) from ValueTime as vtt where vtt.type='Fuel' and vtt.IdSession=s.id) as totalFuel,
+(select ROUND(sum(vtt.value),1) from ValueTime as vtt where vtt.type='DISTANCE' and vtt.IdSession=s.id) as totalDistance,
+(select ROUND(sum(vtt.value),1) from ValueTime as vtt where vtt.type='LOAD' and vtt.IdSession=s.id) as totalLoad,
+(select ROUND(avg(vtt.value),1) from ValueTime as vtt where vtt.type='Fuel' and vtt.IdSession=s.id) as averageFuel,
+(select ROUND(avg(vtt.value),1) from ValueTime as vtt where vtt.type='DISTANCE' and vtt.IdSession=s.id) as averageDistance,
+(select ROUND(avg(vtt.value),1) from ValueTime as vtt where vtt.type='LOAD' and vtt.IdSession=s.id) as averageLoad,
+(select ROUND(avg(vtt.value),1) from ValueTime as vtt where vtt.type='Speed' and vtt.IdSession=s.id) as averageSpeed, 
+(select ROUND(avg(vtt.value),1) from ValueTime as vtt where vtt.type='RPM' and vtt.IdSession=s.id) as averageRpm,    
+(select ROUND(avg(vtt.value),1) from ValueTime as vtt where vtt.type='ALTITUDE' and vtt.IdSession=s.id) as averageAltitude,
 GROUP_CONCAT(DISTINCT v.time order by v.time asc) TIME, GROUP_CONCAT(case when v.type='Fuel' then v.value end ORDER BY v.time asc SEPARATOR ',') 
-FUEL,GROUP_CONCAT(case when v.type='RPM' then v.value end ORDER BY v.time asc SEPARATOR ',') RPM, 
-GROUP_CONCAT(case when v.type='SPEED' then v.value end ORDER BY v.time asc SEPARATOR ',') SPEED, 
-GROUP_CONCAT(case when v.type='ALTITUDE' then v.value end ORDER BY v.time asc SEPARATOR ',') ALTITUDE, 
-GROUP_CONCAT(case when v.type='DISTANCE' then v.value end ORDER BY v.time asc SEPARATOR ',') DISTANCE, 
-GROUP_CONCAT(case when v.type='LOAD' then v.value end ORDER BY v.time asc SEPARATOR ',') LOADED 
-FROM Session as s JOIN Alias as a ON (a.id = s.IdAlias) JOIN ValueTime as v ON (v.IdSession=s.id) JOIN Machine as m on (s.IdMachine=m.Id) WHERE m.name like ? GROUP BY s.id order by s.score desc LIMIT 200";
+FUEL,GROUP_CONCAT(case when v.type='RPM' then ROUND(v.value,1) end ORDER BY v.time asc SEPARATOR ',') RPM, 
+GROUP_CONCAT(case when v.type='SPEED' then ROUND(v.value,1) end ORDER BY v.time asc SEPARATOR ',') SPEED, 
+GROUP_CONCAT(case when v.type='ALTITUDE' then ROUND(v.value,1) end ORDER BY v.time asc SEPARATOR ',') ALTITUDE, 
+GROUP_CONCAT(case when v.type='DISTANCE' then ROUND(v.value,1) end ORDER BY v.time asc SEPARATOR ',') DISTANCE, 
+GROUP_CONCAT(case when v.type='LOAD' then ROUND(v.value,1) end ORDER BY v.time asc SEPARATOR ',') LOADED 
+FROM Session as s JOIN Alias as a ON (a.id = s.IdAlias) JOIN ValueTime as v ON (v.IdSession=s.id) JOIN Machine as m on (s.IdMachine=m.Id) WHERE m.name like ? GROUP BY s.id order by s.score desc LIMIT 20";
 
 $stmt = $connection->prepare($queryy);
 $stmt->bind_param('s', $data);
